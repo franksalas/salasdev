329d4432b08c8424ea2162155771572f7ce60fa9
franksalas <frank.salas@gmail.com>
2018-02-16 22:36:01 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## salasdev
+
+A clean and simple bootstrap4 theme for pelican.
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("static/css"))
+    .pipe(browserSync.stream());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js',
+   'node_modules/jquery/dist/jquery.min.js',
+   'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(gulp.dest("static/js"))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+
+// Move Fonts to src/fonts
+gulp.task('fonts', function() {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('src/fonts'))
+    .pipe(gulp.dest('static/fonts'))
+})
+
+// Move Font Awesome CSS to src/css
+gulp.task('fa', function() {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest("static/css"))
+
+})
+
+
+
+gulp.task('default', ['js', 'fa', 'fonts', 'serve']);
